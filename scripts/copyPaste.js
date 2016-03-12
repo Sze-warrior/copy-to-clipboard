7ad31e86c130bb89a6b6f171158338f31b43bb00
Michael Szeto <szetomj@gmail.com>
2016-03-12 23:43:48 +1300

    initial commit

---
+++ scripts/copyPaste.js
@@ -0,0 +1,9 @@
+var copyTextareaBtn = document.getElementById('js-textareacopybtn');
+
+copyTextareaBtn.addEventListener('click', function() {
+
+  var copyTextarea = document.getElementById('js-copytextarea');
+  copyTextarea.select();
+  var successful = document.execCommand('copy');
+
+});
